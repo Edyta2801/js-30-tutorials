ee5410ef3400cd354148330a0c5c7c17008b5313
Edyta2801 <edyta.szarowska@interia.pl>
2020-03-16 20:16:59 +0100

    add function for progress button

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,7 +6,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelector('.player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 
 // Build Our functions
@@ -30,12 +30,16 @@ function skip() {
 
 function handleRangeUpdate() {
     video[this.name] = this.value;
-}
-function handleProgress(){
-    const percent=(video.currentTime/video.duration)*100;
-    progressBar.getElementsByClassName.flexBasis=`${percent}%`;
-}
+};
 
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e) {
+    const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
 
 
 
@@ -47,4 +51,13 @@ video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
